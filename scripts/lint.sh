b3dbfad6ab942607b1dc2e8970a5644e3da5d4b4
evlekht <ev.lekht@gmail.com>
2024-08-19 22:12:43 +0400

    wip

--- scripts/lint.sh
+++ scripts/lint.sh
@@ -1,15 +1,11 @@
 #!/usr/bin/env bash
 
-set -o errexit
-set -o pipefail
-set -e
+set -euo pipefail
 
 if ! [[ "$0" =~ scripts/lint.sh ]]; then
   echo "must be run from repository root"
   exit 255
 fi
-
-TARGET="./..."
 
 go install -v github.com/golangci/golangci-lint/cmd/golangci-lint@v1.51.2
 golangci-lint run --config .golangci.yml
