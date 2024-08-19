00ae3577d4d83b220f8aa140e5a3cff520fa7028
evlekht <ev.lekht@gmail.com>
2024-08-19 22:04:32 +0400

    fix lint

--- scripts/lint.sh
+++ scripts/lint.sh
@@ -11,9 +11,7 @@ fi
 
 TARGET="./..."
 
-if ! [ -x "$(command -v golangci-lint)" ]; then
-  go install -v github.com/golangci/golangci-lint/cmd/golangci-lint@v1.49.0
-fi
+go install -v github.com/golangci/golangci-lint/cmd/golangci-lint@v1.51.2
 golangci-lint run --config .golangci.yml
 
 echo "ALL SUCCESS!"
